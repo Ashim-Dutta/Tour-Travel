4b20c1f833d9721b66e38fdcef057ccc5104959b
Ashim-Dutta <ashimdutta800@gmail.com>
2023-12-07 10:30:11 +0530

    This website design for tour & travel

---
+++ src/data.js
@@ -0,0 +1,94 @@
+
+const data = [
+
+    {
+
+    id: 1,
+    name: "Agra",
+    info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+    image: "https://lh3.googleusercontent.com/p/AF1QipOIwFX7pMJXtOZ5zEkes4rLws69dgeTYas5C1p4=s1360-w1360-h1020",
+    price: "34527",
+
+    },
+
+
+    {
+
+        id: 2,
+        name: "Jaipur",
+        info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/41/East_facade_Hawa_Mahal_Jaipur_from_ground_level_%28July_2022%29_-_img_01.jpg",
+        price: "34527",
+    
+        },
+
+
+        {
+
+            id: 3,
+            name: "Varanasi",
+            info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+            image: "https://images.adsttc.com/media/images/6486/f324/7870/7215/2fd6/dbf5/large_jpg/varanasis-ghats-the-adaptable-riverscapes-of-india_1.jpg?1686565718",
+            price: "34527",
+        
+            },
+
+
+            {
+
+                id: 4,
+                name: "Assam",
+                info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+                image: "https://images.deccanherald.com/deccanherald%2Fimport%2Fsites%2Fdh%2Ffiles%2Farticleimages%2F2022%2F10%2F20%2Fwomen-harvest-tea-leaves-jorhat-assam-india-1-1155378-1666285132.jpg?w=1200&ar=40%3A21&auto=format%2Ccompress&ogImage=true&mode=crop",
+                price: "34527",
+            
+                },
+
+
+                {
+
+                    id: 5,
+                    name: "Ayodhya",
+                    info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+                    image: "https://etimg.etb2bimg.com/photo/95023595.cms",
+                    price: "34527",
+                
+                    },
+
+
+                    {
+
+                        id: 6,
+                        name: "Prayagraj",
+                        info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+                        image: "https://images1.livehindustan.com/uploadimage/library/2018/10/16/16_9/16_9_1/_1539677259.jpg",
+                        price: "34527",
+                    
+                        },
+
+
+                        {
+
+                            id: 7,
+                            name: "Rajasthan",
+                            info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+                            image: "https://www.andbeyond.com/wp-content/uploads/sites/5/rajasthan-camels-desert-india.gif",
+                            price: "34527",
+                        
+                            },
+
+
+                            {
+
+                                id: 8,
+                                name: "Delhi",
+                                info: "Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+                                image: "https://deih43ym53wif.cloudfront.net/Rajpath-delhi-shutterstock_1195751923.jpg_7647e1aad2.jpg",
+                                price: "34527",
+                            
+                                },
+
+
+]
+
+export default data;
